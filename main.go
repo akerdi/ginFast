20263ec4b830b9ad61bf3bbe60262360d0acb6f0
少鸿 江 <tianxiaoxin001@gmail.com>
2020-04-24 20:29:35 +0800

    hmmm

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/shaohung001/ginFastApp"
+	"log"
 )
 
 func main() {
 	fmt.Println("????????", ginFastApp.InitApp())
 	
 	fmt.Println("kkkkkk" , ginFastApp.AhuangName())
+	err := ginFastApp.Start(ginFastApp.Init(), ":9300")
+	if err != nil {
+		log.Fatal("????", err)
+	}
 }
